ffcc26042f501ba507547824111d8b6f6691f12b
willianchu <willianchu@hotmail.com>
2022-02-12 17:10:11 -0800

    consolidating to pull atualizations from master

--- queries/08_.sql
+++ queries/08_.sql
@@ -1,2 +1,19 @@
-SELECT *
+SELECT AVG(users_results) AS average_result
+FROM (SELECT users.name, SUM(
+    CASE
+        WHEN alternatives.is_correct = true THEN 
+        
+            (SELECT (SELECT 1 / count(*)::DECIMAL AS total_quiz_questions
+            FROM questions
+            WHERE quiz_id = tests.quiz_id) / COUNT(*)::DECIMAL AS answer_weight
+            FROM alternatives -- weight = 1 / number of correct alternatives
+            WHERE is_correct = true AND question_id = answers.question_id) 
+        ELSE 0 
+    END
+) AS users_results
 FROM answers
+INNER JOIN tests ON answers.test_id = tests.id
+INNER JOIN alternatives ON answers.alternative_id = alternatives.id
+INNER JOIN users ON tests.user_id = users.id
+WHERE quiz_id = $1 and alternatives.is_correct = true
+GROUP BY users.name) AS users_results; -- $1 = quiz_id 
