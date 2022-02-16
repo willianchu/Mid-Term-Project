6127859eef6aa61e3a765b2fd3494b098e9d86d7
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-16 14:01:58 -0700

    Main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
 // Client facing scripts here
+$(document).ready(function() {
+  let coll = document.getElementsByClassName("collapsible");
+  let i;
+
+  for (i = 0; i < coll.length; i++) {
+    $(coll[i]).click(function() {
+      console.log("Clicked! ");
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display === "flex") {
+        $(content).slideUp().then (() => content.style.display = "none");
+      } else {
+        $(content).slideDown().then (() => content.style.display = "flex");;
+
+      }
+    });
+  }
+});
