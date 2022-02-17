0fbcffd9043048477b22f5cc469e90b8cb615fd2
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-16 18:01:15 -0700

    Completed all info needed for tables on the main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,35 @@
 // Client facing scripts here
 $(document).ready(function() {
-  let coll = document.getElementsByClassName("collapsible");
+  let collapsibles = document.getElementsByClassName("collapsible");
   let i;
 
-  for (i = 0; i < coll.length; i++) {
-    $(coll[i]).click(function() {
-      console.log("Clicked! ");
+  for (let coll of collapsibles) {
+    $(coll).click(function() {
       this.classList.toggle("active");
       let content = this.nextElementSibling;
       if (content.style.display === "flex") {
         $(content).slideUp().then (() => content.style.display = "none");
       } else {
-        $(content).slideDown().then (() => content.style.display = "flex");;
+        $(content).slideDown().then (() => content.style.display = "flex");
       }
+    });
+  }
+
+  let shareQuizElements = document.getElementsByClassName("copy-quiz-link");
+
+  for (let shareQuiz of shareQuizElements) {
+    $(shareQuiz).click(function() {
+      const link = $($(shareQuiz).parent().prev().children()[0]).attr("href");
+      navigator.clipboard.writeText(`${window.location.host}${link}`);
+    });
+  }
+
+  let shareTestElements = document.getElementsByClassName("copy-test-link");
+
+  for (let shareTest of shareTestElements) {
+    $(shareTest).click(function() {
+      const link = $($(shareTest).parent().prev().children()[0]).attr("href");
+      navigator.clipboard.writeText(`${window.location.host}${link}`);
     });
   }
 });
